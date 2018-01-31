d5d671d927a6690b1a357687534315b1ec5ea43f
Xavier Lange <xrlange@gmail.com>
2018-01-31 12:30:45 -0400

    Bump hyper

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,7 @@ const TEST_BUF : &str = r##"<14>Dec 13 17:45:02 SANTA-CLAUS-W764.blerg.com nxWin
 "##;
 
 fn main() {
-    env_logger::init().unwrap();
+    env_logger::init();
 
     let args: Vec<String> = env::args().collect();
     let mut arg_iter = args.iter();
